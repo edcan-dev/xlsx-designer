a9dfef314d02c1620d26d4ce6ebc33b59968ebb8
edcan-dev <israel.da.queenie@gmail.com>
2024-02-27 16:50:59 -0600

    je

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="container">
       <div className="d-flex justify-content-between mb-3">
         <h1 className='d-flex align-items-center' >XLSX Designer</h1>
-        <img src="/okysoft-logo.jpg" height={150}/>
+        <img src="./okysoft-logo.jpg" height={150}/>
       </div>
 
       <FileUploader></FileUploader>
